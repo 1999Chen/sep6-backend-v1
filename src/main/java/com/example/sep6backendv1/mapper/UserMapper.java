615dca96e99b338b13ede2290ee1ed1a523bd17d
1999Chen <lsy1999lsy@gmail.com>
2022-06-01 19:35:16 +0200

    user controller update

--- src/main/java/com/example/sep6backendv1/mapper/UserMapper.java
+++ src/main/java/com/example/sep6backendv1/mapper/UserMapper.java
@@ -16,7 +16,7 @@ public interface UserMapper extends BaseMapper<User> {
     User selectUserByUsername(String username);
 
     @Insert("insert into users (username,password,nickname) values(#{username},#{password},#{nickname})")
-    @SelectKey(statement = "select last_insert_username()", keyProperty = "user.username", before = false, resultType = int.class)
+    @SelectKey(statement = "select last_insert_id()", keyProperty = "user.username", before = false, resultType = int.class)
     String insertAUser(User user);
 
     User insertUser(String username, String password, String nickname);
